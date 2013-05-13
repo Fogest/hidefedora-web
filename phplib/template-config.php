826ca9fbf11a02351e524581b108214c422c71fb
Fogest <fogestjv@gmail.com>
2013-05-12 21:25:27 -0400

    Adding template config file

---
+++ phplib/template-config.php
@@ -0,0 +1,27 @@
+<?php
+	/* Default Site Properties */
+	define("SITE_URL","/template/");
+	define("DEFAULT_SITE_NAME", "Template");
+	define("DEFAULT_PAGE_TITLE", "Home");
+	define("META_DESCRIPTION", "Template page for easily creating new websites");
+	
+	/* Paths */
+	define("CSS_PATH", SITE_URL."css/");
+	define("JAVASCRIPT_PATH",SITE_URL."js/");
+	define("HTML_PATH",SITE_URL."html/");
+	define("IMAGE_PATH",SITE_URL."img/");
+	
+	/* File Extensions */
+	define("CSS_EXTENSION", ".css");
+	define("JAVASCRIPT_EXTENSION", ".min.js");
+	
+	/* Template Options */
+	define("LOGIN_REGISTER", true); // Whether to use a login/register system.
+	
+	
+	/* Database */
+	define("DB_HOSTNAME", "localhost");
+	define("DB_USERNAME", "root");
+	define("DB_PASSWORD","");
+	define("DB_DATABASE","template");
+?>
