259a8a9281e7efccf3f3d3fa6e63e9bac50620cd
Justin V <fogestjv@gmail.com>
2014-01-24 13:25:59 -0500

    Fixing merge mess

--- html/footer/about.php
+++ html/footer/about.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include_once ("../../setup.php");
 
@@ -50,13 +49,3 @@ bibendum euismod.        </p>    ";
 
 $page->display();
 ?>
-=======
-<?php
-include_once ("../../setup.php");
-
-$page->page_title = 'About';
-$page->page_header = 'About';
-
-$page->display();
-?>
->>>>>>> 876ad2a9a79d854f30d9b2c9d3d0a6b53e273bb3
