1f3599003ea1b03eff695b2450bf905dfb76aabc
Justin Visser <justin@jhvisser.com>
2014-11-29 23:06:46 -0500

    Only send notification emails if more than 1 email. #5

--- mailer.php
+++ mailer.php
@@ -5,22 +5,24 @@ $sql = "SELECT COUNT(*) as numberOfReports FROM `blockedusers` WHERE `hasBeenEma
 $result = $database->execute($sql);
 $result = $result[0]['numberOfReports'];
 
-$to      = 'fogest@live.com';
-$subject = 'There are ' . $result . ' reports to review';
-$body = 'You need to review '. $result . ' items. Check <a href="https://jhvisser.com/hidefedora/html/review/review.php">the review page</a> and review the item(s)!';
+if($result > 0) {
+	$to      = 'fogest@live.com';
+	$subject = 'There are ' . $result . ' reports to review';
+	$body = 'You need to review '. $result . ' items. Check <a href="https://jhvisser.com/hidefedora/html/review/review.php">the review page</a> and review the item(s)!';
 
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= 'Reply-To: admin@jhvisser.com' . "\r\n" .
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'Reply-To: admin@jhvisser.com' . "\r\n" .
 
-// Additional headers
-$headers .= 'From: Hide Fedora <admin@jhvisser.com>' . "\r\n";
+	// Additional headers
+	$headers .= 'From: Hide Fedora <admin@jhvisser.com>' . "\r\n";
 
-if(mail($to, $subject, $body, $headers)) {
-	$table = 'blockedusers';
-	$args['hasBeenEmailed'] = 1;
-	$where['hasBeenEmailed'] = 0;
-	$database->update($table, $args, $where);
+	if(mail($to, $subject, $body, $headers)) {
+		$table = 'blockedusers';
+		$args['hasBeenEmailed'] = 1;
+		$where['hasBeenEmailed'] = 0;
+		$database->update($table, $args, $where);
+	}
 }
 
 ?> 
