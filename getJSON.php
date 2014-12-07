d1f2b111f0b8568f67562ca34fdd21a89797db57
Justin Visser <justin@jhvisser.com>
2014-12-07 13:01:19 -0500

    Adding caching to json file

--- getJSON.php
+++ getJSON.php
@@ -1,6 +1,22 @@
 <?php
 	include_once ("setup.php");
 
+	$cacheDir = 'cache/';
+	$url = $_SERVER["SCRIPT_NAME"];
+	$break = Explode('/', $url);
+	$file = $break[count($break) - 1];
+	$cachefile = $cacheDir.'cached-'.substr_replace($file ,"",-4).'.html';
+	//Every hour update cache.
+	$cachetime = 3600;
+
+	// Serve from the cache if it is younger than $cachetime
+	if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
+	    echo "<!-- Cached copy, generated ".date('H:i', filemtime($cachefile))." -->\n";
+	    include($cachefile);
+	    exit;
+	}
+	ob_start(); // Start the output buffer
+
 	$sql = "SELECT * FROM `blockedusers` WHERE `approvalStatus` = 1\n"
     . "ORDER BY `blockedusers`.`date` ASC";
 
@@ -14,4 +30,9 @@
     $jsonOutput = array("fedoras" => $ids);
 
 	echo json_encode($jsonOutput);
+	// Cache the contents to a file
+	$cached = fopen($cachefile, 'w');
+	fwrite($cached, ob_get_contents());
+	fclose($cached);
+	ob_end_flush(); // Send the output to the browser
 ?>
