9a04966424a0a5d27d334033707d3318d044e814
Justin V <fogestjv@gmail.com>
2014-12-08 00:20:49 -0500

    Changes to mailing

--- mailer.php
+++ mailer.php
@@ -6,16 +6,19 @@ $result = $database->execute($sql);
 $result = $result[0]['numberOfReports'];
 
 if($result > 0) {
-	$to      = 'fogestjv@gmail.com';
+	$to      = 'jhvisser@sympatico.ca';
 	$subject = 'There are ' . $result . ' reports to review';
 	$body = 'You need to review '. $result . ' items. Check <a href="https://jhvisser.com/hidefedora/html/review/review.php">the review page</a> and review the item(s)!';
 
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-	$headers .= 'Reply-To: admin@jhvisser.com' . "\r\n" .
+	$headers = 'From: "Hide Fedora" <admin@jhvisser.com>' . "\r\n";
+	$headers .= 'Reply-To: "Hide Fedora" <admin@jhvisser.com>' . "\r\n";
 
+	$headers  .= 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html;charset=iso-8859-1' . "\r\n";
+
+	$headers .= 'X-Mailer: PHP/' . phpversion(). "\r\n";
+	$headers .= 'Delivery-Date: ' . date("r") . "\r\n";
 	// Additional headers
-	$headers .= 'From: Hide Fedora <admin@jhvisser.com>' . "\r\n";
 
 	if(mail($to, $subject, $body, $headers)) {
 		$table = 'blockedusers';
